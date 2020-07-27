fdc16bd380d15e7092bd7087ee0f668fda15e37c
niranjana94 <60933884+niranjana94@users.noreply.github.com>
2020-07-27 22:49:21 +0530

    call weather data
    https://api.openweathermap.org/data/2.5/weather?q=Bangalore,Karnataka&appid=008ffcc8199c2fd7456426b8b74a39e3
    
    
    {"coord":{"lon":77.6,"lat":12.98},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":298.49,"feels_like":300.8,"temp_min":297.15,"temp_max":300.37,"pressure":1013,"humidity":83},"visibility":6000,"wind":{"speed":3.6,"deg":200},"clouds":{"all":40},"dt":1595866449,"sys":{"type":1,"id":9205,"country":"IN","sunrise":1595810039,"sunset":1595855878},"timezone":19800,"id":1277333,"name":"Bengaluru","cod":200}

--- day1/request.js
+++ day1/request.js
@@ -1,8 +1,8 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
 //var url_string = 'https://restcountries.eu/rest/v2/all';
-var url_string = 'https://dog.ceo/api/breeds/list/all';
-//var url_string = 'https://api.openweathermap.org/data/2.5/weather?q=Bangalore,Karnataka&appid=008ffcc8199c2fd7456426b8b74a39e3';
+//var url_string = 'https://dog.ceo/api/breeds/list/all';
+var url_string = 'https://api.openweathermap.org/data/2.5/weather?q=Bangalore,Karnataka&appid=008ffcc8199c2fd7456426b8b74a39e3';
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET',url_string , true)
 request.send();
